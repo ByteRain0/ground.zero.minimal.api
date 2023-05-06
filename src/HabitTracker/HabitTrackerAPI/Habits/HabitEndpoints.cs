bcb46623da4c8ca25aa8dfaa61dc582aa658a2db
Vasilii Oleinic <Vasilii.Oleinic@endava.com>
2023-05-06 18:10:00 +0300

    Move endpoint logic to specific internal methods and pass them in to the RouteBuilder.

--- src/HabitTracker/HabitTrackerAPI/Habits/HabitEndpoints.cs
+++ src/HabitTracker/HabitTrackerAPI/Habits/HabitEndpoints.cs
@@ -13,34 +13,36 @@ internal static class HabitEndpoints
             .WithOpenApi()
             .WithValidationFilter();
 
-        
-        group.MapPost("/", async ([Validate] Habit habit, IHabitService service, LinkGenerator linkGenerator, HttpContext context) =>
-            {
-                var habitId = await service.AddHabitAsync(habit);
-                var path = linkGenerator.GetUriByName(context, "GetHabit", new {id = habitId})!;
-                return Results.Created(path, habit);
-            })
+        group.MapPost("/", CreateHabitAsync)
             .Accepts<Habit>(Constants.ContentTypes.ApplicationJson)
             .Produces(201)
             .ProducesValidationProblem()
             .WithName("CreateHabit");
-
         
-        group.MapGet("/{id:int}",
-                async (int id, IHabitService service, CancellationToken cancellationToken) =>
-                {
-                    var habit = await service.GetHabitAsync(id, cancellationToken);
-
-                    if (habit is null)
-                    {
-                        return Results.NotFound();
-                    }
-
-                    return Results.Ok(habit);
-                })
+        group.MapGet("/{id:int}", GetHabitByIdAsync)
             .Produces(404)
             .Produces<Habit>()
             .WithName("GetHabit");
+    }
 
+    internal static async Task<IResult> CreateHabitAsync([Validate] Habit habit, IHabitService service,
+        LinkGenerator linkGenerator, HttpContext context)
+    {
+        var habitId = await service.AddHabitAsync(habit);
+        var path = linkGenerator.GetUriByName(context, "GetHabit", new { id = habitId })!;
+        return Results.Created(path, habit);
+    }
+
+    internal static async Task<IResult> GetHabitByIdAsync(int id, IHabitService service,
+        CancellationToken cancellationToken)
+    {
+        var habit = await service.GetHabitAsync(id, cancellationToken);
+
+        if (habit is null)
+        {
+            return Results.NotFound();
+        }
+
+        return Results.Ok(habit);
     }
 }
