b21bc090ecf5d60cc92d6570111cc101fac32d91
vasileoleinic <vasilii.oleinic@indrivo.com>
2023-05-20 13:09:20 +0300

    Add missing Migration.

---
+++ src/HabitTracker/HabitTrackerAPI/Habits/Data/Migrations/20230520100053_DayInformation_RemoveColumn_Checked.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HabitTrackerAPI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DayInformation_RemoveColumn_Checked : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Checked",
+                table: "DaysInformation");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Checked",
+                table: "DaysInformation",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
