c54cf09814eb69720b4f4aec3528dd51bd36adc0
ByteRain0 <oleinicvasea45@gmail.com>
2023-12-13 11:09:23 +0200

    Update IIntegrationTestsBuild.cs
    Make the integration tests step run as first in the pipeline.

--- build/Infrastructure/BuildSteps/IIntegrationTestsBuild.cs
+++ build/Infrastructure/BuildSteps/IIntegrationTestsBuild.cs
@@ -7,10 +7,8 @@ public interface IIntegrationTestsBuild : IBaseBuild
 {
     bool ExecuteIntegrationTests => false;
 
-    // A good alternative would be running the integration tests inside the Dockerfile
-    // or have this step depend on a simple Build step not on DockerBuild.
+    // A good alternative would be running the integration tests inside the Dockerfile.
     Target RunIntegrationTests => _ => _
-        .TryDependsOn<IDockerBuild>(x => x.BuildDockerfileWithArtifacts)
         .OnlyWhenDynamic(() => ExecuteIntegrationTests)
         .WhenSkipped(DependencyBehavior.Execute)
         .Executes(() =>
